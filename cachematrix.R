1f2aea29a8a2d756a46f8dd05c136964451c51e0
Venu Palvai <venugopalrp@gmail.com>
2015-02-15 18:44:14 -0600

    my programmingassignment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,48 @@
 
 ## Write a short comment describing this function
 
+##############
+#The first function, makeVector creates a special "vector", which is really a list containing a function to
+#set the value of the matrix
+#get the value of the matrix
+#set inverse of the matrix
+#get inverese of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
-
+        invMtrx <- NULL
+        set <- function(y) {
+                x <<- y
+                invMtrx <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(I) invMtrx <<- I
+        getInverse <- function() invMtrx
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
+#The following function returns inverse of the matrix from the list passed in argument
+#However, it first checks to see if the inverse already exists.
+#If so, it gets the inverse from the cache and skips the computation. 
+#Otherwise, it calculates the inverse using solve function and sets the value of the 
+#inverse in the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        imatrix <- x$getInverse()
+        if(!is.null(imatrix)) {
+                message("getting cached inverse matrix")
+                return(imatrix)
+        }
+        origmatrix <- x$get()
+        imatrix <- solve(origmatrix, ...)
+        x$setInverse(imatrix)
+        
+        return(imatrix)
 }
+### Examples
+#mt <- makeCacheMatrix(matrix(1:4, 2, 2))
+#mt
+#class(mt)
+#class(mt$set)
+#mt$get()  
+#cacheSolve(mt)
+#cacheSolve(mt)
